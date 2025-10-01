1689d085c8f94a6dcd3fde67faf4abfac059ca9e
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2025-10-01 21:27:40 +0300

    Exercise 11.9

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,13 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('card can be opened', function() {
+    cy.visit('')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
