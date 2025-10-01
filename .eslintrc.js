bf11a0e47cf8cbc7f00242b573555a72811ed05f
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2025-10-01 20:37:49 +0300

    Exercise 11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true
   },
   "extends": [
     "eslint:recommended",
@@ -45,5 +46,5 @@ module.exports = {
     ],
     "no-console": "error",
     "react/prop-types": 0
-  }
+  },
 }
